0a35146212338ade94e17847e04bbc443f3b5e85
LeyreOrtiz <ortiz.161784@e.unavarra.es>
2026-03-16 12:59:36 +0100

    [GREEN] - Añadir código un or en el if que devuelve el número 1

--- src/StringCalculator.php
+++ src/StringCalculator.php
@@ -9,7 +9,7 @@ class StringCalculator
         if(empty($numbers)){
             return 0;
         }
-        else if($numbers == "1"){
+        else if($numbers == "1" || $numbers == "1,"){
             return 1;
         }
 
